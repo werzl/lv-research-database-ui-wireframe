ea98186abc16c73ab4b5bc0cf02a6c2f92231226
Adam Hewitt <adam@houseofhewitt.co.uk>
2021-11-05 17:11:26 +0000

    Delete Securitys.ts

--- src/models/Securitys.ts
+++
@@ -1,9 +0,0 @@
-export interface Security {
-    ticker: string,
-    securityName: string,
-    securityId: number,
-    commands: any[],
-    isCommandVisible: boolean,
-    isLocked: boolean,
-    lockMessage: string
-}
