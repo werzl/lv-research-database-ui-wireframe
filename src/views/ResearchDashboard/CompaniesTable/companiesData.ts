db689c803bc5de844ad1d25a4ac25c2872934dea
Adam Hewitt <adam@houseofhewitt.co.uk>
2021-12-01 23:29:59 +0000

    add new companies

--- src/views/ResearchDashboard/CompaniesTable/companiesData.ts
+++ src/views/ResearchDashboard/CompaniesTable/companiesData.ts
@@ -1,3 +1,8 @@
+import { formatDate, randomFormattedDate } from "../../../common/dateHelpers";
+
+const today = new Date();
+const lastYear = new Date(new Date(today).setFullYear(today.getFullYear()-1));
+
 export const companiesData = {
 	columns: [
 		{
@@ -9,13 +14,13 @@ export const companiesData = {
 		{
 			field: "security",
 			headerName: "Security",
-			width: 120,
+			width: 200,
 			editable: false
 		},
 		{
 			field: "dateAdded",
 			headerName: "Date Added",
-			width: 150,
+			width: 200,
 			editable: false
 		}
 	],
@@ -24,8 +29,37 @@ export const companiesData = {
 			id: "1U1 GR",
 			ticker: "1U1 GR",
 			security: "1&1 AG",
-			dateAdded: new Date().toLocaleString("en-GB")
+			dateAdded: formatDate(today)
+		},
+		{
+			id: "TXG US",
+			ticker: "TXG US",
+			security: "10x Genomics Inc Class A",
+			dateAdded: randomFormattedDate(today, lastYear)
+		},
+		{
+			id: "ONEM US",
+			ticker: "ONEM US",
+			security: "1Life Healthcare Inc",
+			dateAdded: randomFormattedDate(today, lastYear)
+		},
+		{
+			id: "TWOU US",
+			ticker: "TWOU US",
+			security: "2U Inc",
+			dateAdded: randomFormattedDate(today, lastYear)
+		},
+		{
+			id: "2018 HK",
+			ticker: "2018 HK",
+			security: "AAC Technologies Holdings Inc",
+			dateAdded: randomFormattedDate(today, lastYear)
+		},
+		{
+			id: "ANL LN",
+			ticker: "ANL LN",
+			security: "Abbey National Plc",
+			dateAdded: randomFormattedDate(today, lastYear)
 		}
-
 	]
 }
