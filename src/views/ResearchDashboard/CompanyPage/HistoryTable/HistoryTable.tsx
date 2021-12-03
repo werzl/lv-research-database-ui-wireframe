c36c8183793e4404ce8fede1041197241f90fba4
Adam Hewitt <adam@houseofhewitt.co.uk>
2021-12-03 11:18:19 +0000

    fix errors again

--- src/views/ResearchDashboard/CompanyPage/HistoryTable/HistoryTable.tsx
+++ src/views/ResearchDashboard/CompanyPage/HistoryTable/HistoryTable.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import TextField from '@mui/material/TextField';
+//import IconButton from '@mui/material/IconButton';
+//import TextField from '@mui/material/TextField';
 import {
     DataGrid,
     GridColumns,
     GridSortModel,
-    GridToolbarDensitySelector,
-    GridToolbarFilterButton,
+    //GridToolbarDensitySelector,
+    //GridToolbarFilterButton,
 } from '@mui/x-data-grid';
-import ClearIcon from '@mui/icons-material/Clear';
-import SearchIcon from '@mui/icons-material/Search';
+//import ClearIcon from '@mui/icons-material/Clear';
+//import SearchIcon from '@mui/icons-material/Search';
 import { createTheme } from '@mui/material/styles';
-import { createStyles, makeStyles } from '@mui/styles';
+//import { createStyles, makeStyles } from '@mui/styles';
 
 import { historyData } from "./historyData";
 
@@ -19,32 +19,32 @@ function escapeRegExp(value: string) {
     return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
-const defaultTheme = createTheme();
-const useStyles = makeStyles(
-    (theme) =>
-        createStyles({
-            root: {
-                padding: theme.spacing(0.5, 0.5, 0),
-                justifyContent: 'space-between',
-                display: 'flex',
-                alignItems: 'flex-start',
-                flexWrap: 'wrap',
-            },
-            textField: {
-                [theme.breakpoints.down('xs')]: {
-                    width: '100%',
-                },
-                margin: theme.spacing(1, 0.5, 1.5),
-                '& .MuiSvgIcon-root': {
-                    marginRight: theme.spacing(0.5),
-                },
-                '& .MuiInput-underline:before': {
-                    borderBottom: `1px solid ${theme.palette.divider}`,
-                },
-            },
-        }),
-    { defaultTheme },
-);
+//const defaultTheme = createTheme();
+// const useStyles = makeStyles(
+//     (theme) =>
+//         createStyles({
+//             root: {
+//                 padding: theme.spacing(0.5, 0.5, 0),
+//                 justifyContent: 'space-between',
+//                 display: 'flex',
+//                 alignItems: 'flex-start',
+//                 flexWrap: 'wrap',
+//             },
+//             textField: {
+//                 [theme.breakpoints.down('xs')]: {
+//                     width: '100%',
+//                 },
+//                 margin: theme.spacing(1, 0.5, 1.5),
+//                 '& .MuiSvgIcon-root': {
+//                     marginRight: theme.spacing(0.5),
+//                 },
+//                 '& .MuiInput-underline:before': {
+//                     borderBottom: `1px solid ${theme.palette.divider}`,
+//                 },
+//             },
+//         }),
+//     { defaultTheme },
+// );
 
 // function QuickSearchToolbar(props: any) {
 //     const classes = useStyles();
