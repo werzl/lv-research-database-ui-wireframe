b30a83356a64002692be78022443a9da31239a6a
Adam Hewitt <adam@houseofhewitt.co.uk>
2021-12-21 17:22:43 +0000

    rewrite ResearchEntriesTable to include attachment details

--- src/views/CompanyPage/Tabs/ResearchEntries/ResearchEntriesTable.tsx
+++ src/views/CompanyPage/Tabs/ResearchEntries/ResearchEntriesTable.tsx
@@ -1,44 +1,121 @@
-import * as React from 'react';
-import {
-    DataGrid,
-    GridColumns,
-    GridSortModel,
-} from '@mui/x-data-grid';
+import React, { useState } from "react";
+
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Collapse, Tooltip } from "@mui/material";
+import { Button, Col, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+
+import { TResearchEntry } from "../../../../types/ResearchEntry";
+import { formatDate } from "../../../../common/dateHelpers";
+import { getCompanyNameFromUrl } from "../../../../common/urlHelper";
 
 import { researchEntriesData } from "./researchEntriesData";
 
-export interface HistoryTableProps {
-    companyName: string
-}
-
-export default function ResearchEntriesTable(props: HistoryTableProps) {
-    const data = researchEntriesData;
-
-    const [rows, setRows] = React.useState(data.rows);
-    const [sortModel, setSortModel] = React.useState<GridSortModel>([
-        {
-            field: 'date',
-            sort: 'desc',
-        },
-    ]);
-
-    React.useEffect(() => {
-        setRows(data.rows);
-    }, [data.rows]);
+const ResearchEntryRow = (props: TResearchEntry) => {
+    const [open, setOpen] = useState<boolean>(false);
 
     return (
-        <div className="companyPage-historyTable-dataGrid">
-            <DataGrid
-                className="dataGrid"
-                rows={rows}
-                columns={data.columns as GridColumns}
-                columnBuffer={data.columns.length ?? 5}
-                hideFooterSelectedRowCount
-                hideFooterPagination
-                disableSelectionOnClick
-                sortModel={sortModel}
-                onSortModelChange={(model) => setSortModel(model)}
-                density="compact"/>
+        <>
+            <TableRow>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+
+                <TableCell>{props.sourceId}</TableCell>
+
+                <TableCell>
+                    <Link to={`/${getCompanyNameFromUrl()}/${props.sourceId}`}>{formatDate(props.date)}</Link>
+                </TableCell>
+
+                <TableCell className="ps-5">{props.attachments ? props.attachments.count : 0}</TableCell>
+
+                <TableCell>
+                    <p className="comments">
+                        <Tooltip title={props.comments ?? ""} classes={{ popper: "companyPage-historyTable-comments-tooltip" }} placement="bottom-end">
+                            <div className="companyPage-historyTable-comments-cell">{props.comments}</div>
+                        </Tooltip>
+                    </p>
+                </TableCell>
+
+                <TableCell></TableCell>
+            </TableRow>
+
+            <TableRow>
+                <TableCell colSpan={6} className="p-0">
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Row className="m-5 mt-3">
+                            <Col>
+                                <h5 className="m-3 text-secondary">Attachments</h5>
+
+                                <Table className="attachments-table w-50">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Filename</TableCell>
+                                            <TableCell>Type</TableCell>
+                                            <TableCell>Description</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+
+                                    <TableBody>
+                                        {props.attachments ? props.attachments.list.map(attachment => (
+                                            <TableRow>
+                                                <TableCell><a href={window.location.href}>{attachment.filename}</a></TableCell>
+                                                <TableCell>{attachment.type}</TableCell>
+                                                <TableCell>{attachment.description}</TableCell>
+                                            </TableRow>
+                                        )) : <></>}
+                                    </TableBody>
+                                </Table>
+                            </Col>
+                        </Row>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    )
+}
+
+const ResearchEntriesTable = () => {
+    return (
+        <div className="companyPage-historyTable-researchEntriesTable-container">
+            <Table size="small" className="w-100">
+                <TableHead>
+                    <TableRow>
+                        <TableCell className="collapse-icon-header"></TableCell>
+                        <TableCell className="sourceId-header">Source ID</TableCell>
+                        <TableCell className="date-header">Date</TableCell>
+                        <TableCell className="attachments-header">Total Attachments</TableCell>
+                        <TableCell className="comments-header">Comments</TableCell>
+                        <TableCell className="newEntry-header">
+                            <LinkContainer to={`/${getCompanyNameFromUrl()}/NewResearchEntry`}>
+                                <Button className="newEntryButton" variant="outline-primary">+</Button>
+                            </LinkContainer>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {researchEntriesData.map(researchEntry => (
+                        <ResearchEntryRow {...researchEntry} />
+                    ))}
+                </TableBody>
+            </Table>
         </div>
     );
-}
+};
+
+export default ResearchEntriesTable;
--- src/views/CompanyPage/Tabs/ResearchEntries/researchEntriesData.tsx
+++ src/views/CompanyPage/Tabs/ResearchEntries/researchEntriesData.tsx
@@ -1,120 +1,98 @@
-import { Tooltip } from "@mui/material";
-import { GridRenderCellParams, GridValueGetterParams } from "@mui/x-data-grid";
-import { Button } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { Link } from "react-router-dom";
-import { formatDate, randomDate } from "../../../../common/dateHelpers";
-import { getCompanyNameFromUrl } from "../../../../common/urlHelper";
+import { TResearchEntry } from "../../../../types/ResearchEntry";
+
+import { randomDate } from "../../../../common/dateHelpers";
 
 const today = new Date();
 const lastYear = new Date(new Date(today).setFullYear(today.getFullYear() - 1));
 
-
-export const researchEntriesData = {
-    columns: [
-        {
-            field: "sourceId",
-            headerName: "Source ID",
-            width: 125,
-            editable: false
+export const researchEntriesData: TResearchEntry[] = [
+    {
+        sourceId: "sourceId1",
+        date: today,
+        attachments: {
+            count: 1,
+            list: [
+                {
+                    filename: "test.docx",
+                    type: "NonStandard Model",
+                    description: ""
+                }
+            ]
         },
-        {
-            field: "date",
-            headerName: "Date",
-            width: 125,
-            editable: false,
-            valueFormatter: (params: GridValueGetterParams) => {
-                return formatDate(params.value);
-            },
-            renderCell: (params: GridRenderCellParams<string>) => {
-                return (
-                    <Link to={`/${getCompanyNameFromUrl()}/${params.row.id}`}>{params.formattedValue}</Link>
-                )
-            }
+        comments: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque facilisis, risus non finibus commodo, erat purus finibus risus, sed blandit dolor magna quis dui. Pellentesque eget rutrum urna. Praesent rutrum, sem non sagittis bibendum, tellus nisi aliquet augue, eu blandit ex metus id nisl. Nulla et lobortis mauris. Nam molestie arcu id tellus lobortis euismod. Nunc eget commodo neque. Integer sollicitudin dignissim ante, sed venenatis lectus semper non. Sed at luctus ligula."
+    },
+    {
+        sourceId: "sourceId2",
+        date: randomDate(today, lastYear),
+        attachments: {
+            count: 1,
+            list: [
+                {
+                    filename: "test.docx",
+                    type: "NonStandard Model",
+                    description: ""
+                }
+            ]
         },
-        {
-            field: "attachments",
-            headerName: "Attachments",
-            width: 100,
-            headerClassName: "companyPage-historyTable-attachments",
-            cellClassName: "companyPage-historyTable-attachments",
-            sortable: false,
-            filterable: false,
-            disableColumnMenu: true
+        comments: "Vivamus orci tortor, elementum vitae orci dignissim, congue facilisis turpis."
+    },
+    {
+        sourceId: "sourceId3",
+        date: randomDate(today, lastYear),
+        attachments: {
+            count: 1,
+            list: [
+                {
+                    filename: "test.docx",
+                    type: "NonStandard Model",
+                    description: ""
+                }
+            ]
         },
-        {
-            field: "comments",
-            headerName: "Comments",
-            width: 500,
-            sortable: false,
-            filterable: false,
-            disableColumnMenu: true,
-            renderCell: (params: GridRenderCellParams<string>) => {
-                return (
-                    <Tooltip title={params.row.comments ?? ""} classes={{ popper: "companyPage-historyTable-comments-tooltip" }} placement="bottom-end">
-                        <div className="companyPage-historyTable-comments-cell">{params.row.comments}</div>
-                    </Tooltip>
-                )
-            }
+        comments: "Pellentesque vitae ipsum ligula. Nam imperdiet, urna ut consequat varius, tellus ante dignissim ex, porttitor luctus nibh erat id lectus. Nullam ac lectus et dui ultricies maximus ut ac nibh. Suspendisse potenti. Sed tempor venenatis cursus. Phasellus urna lorem, vulputate id bibendum dictum, tristique vel nisi. Aliquam diam ex, euismod at placerat sit amet, vehicula sed diam. Proin in risus diam. Duis id ex ex. Nullam finibus, leo at pulvinar varius, nibh nulla sollicitudin nisi, id ullamcorper augue massa sit amet felis. Sed interdum, tortor ac ornare imperdiet, augue sem laoreet lacus, et dictum mi turpis non metus. Duis volutpat dolor at felis vulputate, at eleifend nisl lacinia. Sed eget fringilla mauris, id maximus enim. Nam dignissim, arcu vel tincidunt ultrices, justo nisi accumsan ipsum, quis accumsan libero ante vitae dui. Duis a consequat lorem."
+    },
+    {
+        sourceId: "sourceId4",
+        date: randomDate(today, lastYear),
+        attachments: {
+            count: 1,
+            list: [
+                {
+                    filename: "test.docx",
+                    type: "NonStandard Model",
+                    description: ""
+                }
+            ]
         },
-        {
-            field: "newEntryButton",
-            width: 425,
-            sortable: false,
-            resizable: false,
-            disableColumnMenu: true,
-            filterable: false,
-            headerAlign: "right",
-            renderHeader: (params: any) => {
-                return (
-                    <LinkContainer to={`/${getCompanyNameFromUrl()}/NewResearchEntry`}>
-                        <Button className="companyPage-historyTable-newEntryButton" variant="outline-primary">+</Button>
-                    </LinkContainer>
-                )
-            }
-        }
-    ],
-    rows: [
-        {
-            id: "1",
-            sourceId: "sourceId1",
-            date: today,
-            attachments: 1,
-            comments: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque facilisis, risus non finibus commodo, erat purus finibus risus, sed blandit dolor magna quis dui. Pellentesque eget rutrum urna. Praesent rutrum, sem non sagittis bibendum, tellus nisi aliquet augue, eu blandit ex metus id nisl. Nulla et lobortis mauris. Nam molestie arcu id tellus lobortis euismod. Nunc eget commodo neque. Integer sollicitudin dignissim ante, sed venenatis lectus semper non. Sed at luctus ligula."
+        comments: "Duis eu eleifend erat. Nunc vitae efficitur velit, aliquet tristique nunc. Sed et finibus dolor. Nullam gravida cursus efficitur. Integer aliquet rhoncus dolor nec congue. Phasellus placerat urna at est gravida sagittis in id velit. Morbi malesuada finibus mauris sed ullamcorper. Aliquam sapien arcu, dignissim eget condimentum ut, varius vel dui. Aenean scelerisque urna in rutrum sollicitudin. Nullam dictum leo diam, a vulputate odio consequat eget. Sed a pulvinar ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis augue ante, vestibulum ut volutpat ut, iaculis a erat. Donec gravida semper justo non congue. Maecenas bibendum nulla orci, et suscipit turpis tempus in. Nunc dolor mi, pulvinar eu nisi eget, pretium auctor massa."
+    },
+    {
+        sourceId: "sourceId5",
+        date: randomDate(today, lastYear),
+        attachments: {
+            count: 1,
+            list: [
+                {
+                    filename: "test.docx",
+                    type: "NonStandard Model",
+                    description: ""
+                }
+            ]
         },
-        {
-            id: "2",
-            sourceId: "sourceId2",
-            date: randomDate(today, lastYear),
-            attachments: 2,
-            comments: "Vivamus orci tortor, elementum vitae orci dignissim, congue facilisis turpis."
+        comments: "Nulla eget accumsan nisl, eget maximus nulla."
+    },
+    {
+        sourceId: "sourceId6",
+        date: randomDate(today, lastYear),
+        attachments: {
+            count: 1,
+            list: [
+                {
+                    filename: "test.docx",
+                    type: "NonStandard Model",
+                    description: ""
+                }
+            ]
         },
-        {
-            id: "3",
-            sourceId: "sourceId3",
-            date: randomDate(today, lastYear),
-            attachments: 0,
-            comments: "Pellentesque vitae ipsum ligula. Nam imperdiet, urna ut consequat varius, tellus ante dignissim ex, porttitor luctus nibh erat id lectus. Nullam ac lectus et dui ultricies maximus ut ac nibh. Suspendisse potenti. Sed tempor venenatis cursus. Phasellus urna lorem, vulputate id bibendum dictum, tristique vel nisi. Aliquam diam ex, euismod at placerat sit amet, vehicula sed diam. Proin in risus diam. Duis id ex ex. Nullam finibus, leo at pulvinar varius, nibh nulla sollicitudin nisi, id ullamcorper augue massa sit amet felis. Sed interdum, tortor ac ornare imperdiet, augue sem laoreet lacus, et dictum mi turpis non metus. Duis volutpat dolor at felis vulputate, at eleifend nisl lacinia. Sed eget fringilla mauris, id maximus enim. Nam dignissim, arcu vel tincidunt ultrices, justo nisi accumsan ipsum, quis accumsan libero ante vitae dui. Duis a consequat lorem."
-        },
-        {
-            id: "4",
-            sourceId: "sourceId4",
-            date: randomDate(today, lastYear),
-            attachments: 3,
-            comments: "Duis eu eleifend erat. Nunc vitae efficitur velit, aliquet tristique nunc. Sed et finibus dolor. Nullam gravida cursus efficitur. Integer aliquet rhoncus dolor nec congue. Phasellus placerat urna at est gravida sagittis in id velit. Morbi malesuada finibus mauris sed ullamcorper. Aliquam sapien arcu, dignissim eget condimentum ut, varius vel dui. Aenean scelerisque urna in rutrum sollicitudin. Nullam dictum leo diam, a vulputate odio consequat eget. Sed a pulvinar ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis augue ante, vestibulum ut volutpat ut, iaculis a erat. Donec gravida semper justo non congue. Maecenas bibendum nulla orci, et suscipit turpis tempus in. Nunc dolor mi, pulvinar eu nisi eget, pretium auctor massa."
-        },
-        {
-            id: "5",
-            sourceId: "sourceId5",
-            date: randomDate(today, lastYear),
-            attachments: 2,
-            comments: "Nulla eget accumsan nisl, eget maximus nulla."
-        },
-        {
-            id: "6",
-            sourceId: "sourceId6",
-            date: randomDate(today, lastYear),
-            attachments: 4
-        }
-    ]
-}
+    }
+];
