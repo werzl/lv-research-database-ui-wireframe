07ab7758ad64ee32f290a15c9b5e381b1df94daf
Adam Hewitt <adam@houseofhewitt.co.uk>
2021-12-21 17:21:54 +0000

    Create ResearchEntry.ts

---
+++ src/types/ResearchEntry.ts
@@ -0,0 +1,17 @@
+export type Attachment = {
+    filename: string,
+    type: "Above Average" | "Below Average" | "Top 10" | "Top 10 Percentile"
+    | "Bottom 10" | "Bottom 10 Percentile" | "ESG" | "NonStandard Model"
+    | "Note" | "Other"
+    description: string
+}
+
+export type TResearchEntry = {
+    sourceId: string,
+    date: Date,
+    attachments?: {
+        count: number,
+        list: Attachment[]
+    },
+    comments?: string
+}
