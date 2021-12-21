5e98c337279fba15f74c04279a08c3ec359ce037
Adam Hewitt <adam@houseofhewitt.co.uk>
2021-12-21 17:22:06 +0000

    Delete Attachment.ts

--- src/types/Attachment.ts
+++
@@ -1,7 +0,0 @@
-export type Attachment = {
-    filename: string,
-    type: "Above Average" | "Below Average" | "Top 10" | "Top 10 Percentile"
-        | "Bottom 10" | "Bottom 10 Percentile" | "ESG" | "NonStandard Model"
-        | "Note" | "Other"
-    description: string
-}
