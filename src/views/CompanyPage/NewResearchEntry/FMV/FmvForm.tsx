ce9e5e5011841cf15fa84d9b55b18e65d241841c
Adam Hewitt <adam@houseofhewitt.co.uk>
2022-01-10 17:20:39 +0000

    Create FmvForm.tsx

---
+++ src/views/CompanyPage/NewResearchEntry/FMV/FmvForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+const FmvForm = () => {
+    return (
+        <Row>
+            <Col>
+                FMV
+            </Col>
+        </Row>
+    )
+}
+
+export default FmvForm;
