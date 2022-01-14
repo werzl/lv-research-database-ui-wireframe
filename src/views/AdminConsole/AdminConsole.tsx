408a48fd5eb39973443e558e2c233d23a0080a65
Adam Hewitt <adam@houseofhewitt.co.uk>
2022-01-14 17:26:45 +0000

    add admintable

--- src/views/AdminConsole/AdminConsole.tsx
+++ src/views/AdminConsole/AdminConsole.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import AdminTable from "./AdminTable/AdminTable";
+
+import "./AdminConsole.scss";
 
 const AdminConsole = () => {
     return (
-        <p>Admin Console.</p>
+        <Container className="mt-5 h-100">
+            <Row>
+                <Col className="text-center">
+                    <h4>Admin Console</h4>
+                </Col>
+            </Row>
+
+            <Row className="mt-5">
+                <Col>
+                    <AdminTable />
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
---
+++ src/views/AdminConsole/AdminTable/AdminTable.tsx
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import {
+	DataGrid,
+	GridColumns
+} from '@mui/x-data-grid';
+import ClearIcon from '@mui/icons-material/Clear';
+import SearchIcon from '@mui/icons-material/Search';
+import { createTheme } from '@mui/material/styles';
+import { createStyles, makeStyles } from '@mui/styles';
+
+import { adminTableData } from "./adminTableData";
+
+function escapeRegExp(value: string) {
+	return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+const defaultTheme = createTheme();
+const useStyles = makeStyles(
+	(theme) =>
+		createStyles({
+			root: {
+				padding: theme.spacing(0.5, 0.5, 0),
+				justifyContent: 'space-between',
+				display: 'flex',
+				alignItems: 'flex-start',
+				flexWrap: 'wrap',
+			},
+			textField: {
+				[theme.breakpoints.down('xs')]: {
+					width: '100%',
+				},
+				margin: theme.spacing(1, 0.5, 1.5),
+				'& .MuiSvgIcon-root': {
+					marginRight: theme.spacing(0.5),
+				},
+				'& .MuiInput-underline:before': {
+					borderBottom: `1px solid ${theme.palette.divider}`,
+				},
+			},
+		}),
+	{ defaultTheme },
+);
+
+function QuickSearchToolbar(props: any) {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<div></div>
+			<TextField
+				variant="standard"
+				value={props.value}
+				onChange={props.onChange}
+				placeholder="Search…"
+				className={classes.textField}
+				InputProps={{
+					startAdornment: <SearchIcon fontSize="small" />,
+					endAdornment: (
+						<IconButton
+							title="Clear"
+							aria-label="Clear"
+							size="small"
+							style={{ visibility: props.value ? 'visible' : 'hidden' }}
+							onClick={props.clearSearch}
+						>
+							<ClearIcon fontSize="small" />
+						</IconButton>
+					),
+				}}
+			/>
+		</div>
+	);
+}
+
+export default function AdminTable() {
+	// const { data } = useDemoData({
+	// 	dataSet: 'Commodity',
+	// 	rowLength: 100,
+	// 	maxColumns: 6,
+	// });
+
+	const data = adminTableData;
+
+	const [searchText, setSearchText] = React.useState('');
+	const [rows, setRows] = React.useState(data.rows);
+
+	const requestSearch = (searchValue: string) => {
+		setSearchText(searchValue);
+		const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
+		const filteredRows = data.rows.filter((row: any) => {
+			return Object.keys(row).some((field) => {
+				return searchRegex.test(row[field].toString());
+			});
+		});
+		setRows(filteredRows);
+	};
+
+	React.useEffect(() => {
+		setRows(data.rows);
+	}, [data.rows]);
+
+	return (
+		<div className="adminConsole-AdminTable-dataGrid">
+			<DataGrid
+				components={{ Toolbar: QuickSearchToolbar }}
+				rows={rows}
+				columns={data.columns as GridColumns}
+				componentsProps={{
+					toolbar: {
+						value: searchText,
+						onChange: (event: any) => requestSearch(event.target.value),
+						clearSearch: () => requestSearch(''),
+					},
+				}}
+				hideFooterPagination
+				hideFooterSelectedRowCount
+				disableSelectionOnClick
+				density="compact" />
+		</div>
+	);
+}
